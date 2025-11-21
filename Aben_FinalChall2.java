72650e19cf6e58058a31149098fb69cec513a104
Disappointed-Asian <zeiusaben1@gmail.com>
2025-11-21 22:06:30 +0800

    Update Aben_FinalChall2.java

--- Aben_FinalChall2.java
+++ Aben_FinalChall2.java
@@ -23,6 +23,7 @@ public class Aben_FinalChall2 {
         };
         //Temperature of each city in a week
         float[][] temperature = new float[3][7];
+
         //Average temperature of each city
         float[] averageTemp = new float[3];
 
@@ -30,11 +31,26 @@ public class Aben_FinalChall2 {
         System.out.println("\t\tWEATHER DATA TRACKER");
         System.out.println("==========================================================");
 
+        float userInput = 0;
+
+        //Records temperature data for each city
         for (int cityIndex = 0; cityIndex < city.length; cityIndex++) {
             System.out.println("Recording Temperature in: " + city[cityIndex]);
             for (int tempIndex = 0; tempIndex < temperature[cityIndex].length; tempIndex++) {
+
+                //Checks if user inputs correctly
+                try{
                 System.out.print(day[tempIndex] + ": \t");
-                temperature[cityIndex][tempIndex] = sc.nextFloat();
+                userInput = sc.nextFloat();}
+                catch(InputMismatchException e){
+                    System.out.println("Please type a number.");
+                    tempIndex--;
+                    sc.next();
+                    continue;
+                }
+
+                //Stores temperature data on current city
+                temperature[cityIndex][tempIndex] = userInput;
             }
             System.out.println("---------------------------------------------------------");
         }
@@ -52,17 +68,22 @@ public class Aben_FinalChall2 {
             calculatedTemp = 0;
         }
 
+        //Prints the results
         for (int cityIndex = 0; cityIndex < 3; cityIndex++) {
             System.out.println("\n==========================================================");
             System.out.println("\t\t" + city[cityIndex]);
             System.out.println("==========================================================");
-            for (int tempIndex = 0; tempIndex < 7; tempIndex++) {
+
+            for (int tempIndex = 0; tempIndex < temperature[cityIndex].length; tempIndex++) {
                 System.out.println(day[tempIndex] + ": \t" + temperature[cityIndex][tempIndex]);
-            }
+            };
+
             float[] temps = temperature[cityIndex].clone();
             Arrays.sort(temps);
+
             System.out.println("Average Temperature: " + averageTemp[cityIndex]);
             System.out.println("Highest Temperature: " + temps[temps.length - 1]);
+            System.out.println("Lowest Temperature: " + temps[0]);
         }
         System.out.println("\n---------------------------------------------------------");
     }
