783df54d3ba98b1d495ab4ba8a2efef99424ef7e
Disappointed-Asian <zeiusaben1@gmail.com>
2026-03-29 21:34:24 +0800

    Update Graphs.java

--- Graphs.java
+++ Graphs.java
@@ -1,12 +1,17 @@
-import java.util.*;
+package com.example;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Type in the edges. (Type STOP to stop).");
         int[] num = new int[2];
         ArrayList<Vertex> graph = new ArrayList<>();
 
+        //Creating the tree
         while(true){
             String userInput =  scan.next();
 
@@ -27,7 +32,39 @@ public class Main {
             connectVertex(num, graph);
         }
 
-        for(Vertex vertex: graph)
+        //Lowest or Highest Order Priority
+        while(true){
+            System.out.println("1. Lowest Order\n2. Highest Order");
+            String userOrder = scan.nextLine();
+            if(userOrder.equalsIgnoreCase("1")){
+                for(Vertex vertex: graph)
+                    vertex.adj.sort(Comparator.comparing(Vertex::getValue));
+                break;
+            } else if(userOrder.equalsIgnoreCase("2")){
+                for(Vertex vertex: graph)
+                    vertex.adj.sort(Comparator.comparing(Vertex::getValue).reversed());
+                break;
+            }
+            System.out.println("Invalid input.");
+        }
+
+        Vertex root;
+        //Identify root
+        while (true){
+            System.out.println("Enter root");
+            int userRoot = scan.nextInt();
+            root = getVertex(userRoot, graph);
+            if(root == null){
+                continue;
+            }
+            break;
+        }
+
+        ArrayList<Vertex> dfs = new ArrayList<>();
+        dfs.add(root);
+        createDFS(root, dfs, graph);
+
+        for(Vertex vertex: dfs)
             System.out.println(vertex.value);
 
         scan.close();
@@ -46,22 +83,47 @@ public class Main {
     //Which connects to which
     static void connectVertex(int[] num, ArrayList<Vertex> graph){
         for(Vertex vertex: graph){
+            if (vertex.adj == null)
+                vertex.adj.add(getVertex(num[1], graph));
+
             if(vertex.value == num[0]){
-                if(!vertex.adj.contains(num[1]))
-                    vertex.adj.add(num[1]);
+                if(!vertex.adj.contains(getVertex(num[1], graph)))
+                    vertex.adj.add(getVertex(num[1], graph));
             }
 
             if(vertex.value == num[1])
-                if(!vertex.adj.contains(num[0]))
-                    vertex.adj.add(num[0]);
+                if(!vertex.adj.contains(getVertex(num[0], graph)))
+                    vertex.adj.add(getVertex(num[0], graph));
+        }
+    }
+
+    static Vertex getVertex(int value, ArrayList<Vertex> graph){
+        for(Vertex vertex: graph){
+            if(vertex.value == value)
+                return vertex;
+        }
+        System.out.println("--------Vertex does not exist.--------");
+        return null;
+    }
+
+    static void createDFS(Vertex root, ArrayList<Vertex> dfs, ArrayList<Vertex> graph){
+        for(Vertex vertex: root.adj){
+            if(!dfs.contains(vertex)){
+                dfs.add(vertex);
+                createDFS(vertex, dfs, graph);
+            }
         }
     }
 }
 
 class Vertex{
     int value;
-    ArrayList<Integer> adj;
+    ArrayList<Vertex> adj = new ArrayList<>();
     Vertex(int value){
         this.value = value;
+    }
+
+    public int getValue(){
+        return this.value;
     }
 }
